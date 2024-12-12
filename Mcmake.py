1495f8c0d9fe74413865886a33533370a7808e5b
mdstm <shdaed@163.com>
2024-12-12 16:52:26 +0800

    add Mcmake.py

---
+++ Mcmake.py
@@ -0,0 +1,38 @@
+'''使用 cmake 配置并构建项目'''
+
+import argparse
+import os
+
+
+def main():
+    p = argparse.ArgumentParser(description=__doc__,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    p.add_argument('source', nargs='?', default='.', help='源路径')
+    p.add_argument('build', nargs='?', default='build', help='构建路径')
+    p.add_argument('-r', '--release', action='store_true', help='发行版')
+    p.add_argument('-o', '--other', default='', help='其他 cmake 参数')
+    p.add_argument('-c', '--clean', action='store_true', help='删除原构建')
+
+    args = p.parse_args()
+
+    build = args.build
+
+    if args.clean and os.path.exists(build):
+        from shutil import rmtree
+        rmtree(build)
+
+    cmd = f'cmake -G Ninja -S "{args.source}" -B "{build}"'\
+        ' -DCMAKE_INSTALL_PREFIX=install'\
+        ' -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++'\
+        ' -DCMAKE_EXPORT_COMPILE_COMMANDS=ON'\
+        f' -DCMAKE_BUILD_TYPE={(
+            'Release' if args.release else
+            'Debug -DCMAKE_VERBOSE_MAKEFILE=ON'
+        )} {args.other} && cmake --build "{build}"'
+
+    print(cmd)
+    os.system(cmd)
+
+
+if __name__ == '__main__':
+    main()
