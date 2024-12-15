b1c9f5c6d58aa0fa9dda661b8ecf5e661bd7c3fa
mdstm <shdaed@163.com>
2024-12-15 15:32:33 +0800

    修改格式

--- Mhash.py
+++ Mhash.py
@@ -6,7 +6,8 @@ from hashlib import file_digest, algorithms_available
 
 def main():
     p = argparse.ArgumentParser(description=__doc__,
-        epilog='可用的 hash 算法有: ' + ', '.join(algorithms_available),formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        epilog='可用的 hash 算法有: ' + ', '.join(algorithms_available),
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     p.add_argument('file', nargs='+', help='文件名称')
     p.add_argument('-a', '--algorithm', default='sha256', help='hash 算法')
 
